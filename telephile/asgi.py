4e125226b1bb9cd382702719aaed9454e86f69be
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-05-04 20:13:29 +0000

    set up django deployment

---
+++ telephile/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for telephile project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telephile.settings')
+
+application = get_asgi_application()
