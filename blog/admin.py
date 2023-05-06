0a0b63332660be4e3d93e1d9cd05c42ca2e1309f
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-05-06 13:55:47 +0000

    added prepoulated slug field to admin

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,5 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     
+    prepopulated_fields = {'slug': ('series',)}
     summernote_fields = ('content')
