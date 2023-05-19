99adbb774268e9e649bfeab62d855ab1fe18b7d9
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-05-19 13:38:17 +0000

    added styling and media queries

--- telephile/settings.py
+++ telephile/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['telephile.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['telephile.herokuapp.com', 'localhost', '8000-mcdaid101-telephile-cg1g8685994.ws-eu97.gitpod.io']
 
 
 # Application definition
