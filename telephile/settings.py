171552bff7a77ee347ff5f15b0fcd40e0ca1bc75
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-05-22 08:30:10 +0000

    set debug to false and submitting

--- telephile/settings.py
+++ telephile/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['telephile.herokuapp.com', 'localhost', '8000-mcdaid101-telephile-cg1g8685994.ws-eu97.gitpod.io']
 
