669bedfc95b34196b23c11933ce8b0514dc6cfe1
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-08-23 17:42:06 +0000

    turned debug off

--- telephile/settings.py
+++ telephile/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['telephile.herokuapp.com', 'localhost', '8000-mcdaid101-telephile-x0g7ti76wsg.ws-eu104.gitpod.io']
 
